8060d36957735153dd5135644f264af3df84b8f1
arian caviglia <ariancaviglia7@gmail.com>
2020-07-28 12:41:38 -0300

    Garage controller, garage basic validations, basic routes

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,2 +1,46 @@
 class GaragesController < ApplicationController
+  before_action :set_garage, only: %i(show edit update destroy)
+  
+  def index
+    @garages = Garage.all
+  end
+
+  def show
+  end
+
+  def new
+    @garage = Garage.new
+  end
+
+  def create
+    @garage = Garage.new(garage_params)
+    if @garage.save
+      redirect_to garage_path(@garage)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @garage.update(garage_params)
+    redirect_to :garage_path(@garage)
+  end
+
+  def destroy
+    @garage.destroy
+    redirect_to :garage_path 
+  end
+
+  private
+
+  def garage_params
+    params.require(:garage).permit(%i(title description address availability phone_number dimensions price_per_day price_per_hour))
+  end
+
+  def set_garage
+    @garage = Garage.find(params[:id])
+  end
 end
--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,3 +1,6 @@
 class Garage < ApplicationRecord
   belongs_to :user
+  validates %i(title description address availability phone_number dimensions price_per_day price_per_hour), presence: true
+  validates %i(title address), uniqueness: true
+  has_one_attached :photo
 end
