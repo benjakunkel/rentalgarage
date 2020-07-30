4d6ee206a0cf4262da2daa744362c73f111c2fb2
ayelen gora <ayegora@hotmail.com>
2020-07-30 15:46:39 -0300

    mapa sin estilo

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_214269) do
-
+ActiveRecord::Schema.define(version: 2020_07_30_153910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
