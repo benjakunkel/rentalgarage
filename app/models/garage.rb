f095ab3f5f2fc755506a712869101b2cd0893bde
nicolas guelman <guelmannicolas@gmail.com>
2020-07-31 12:41:35 -0300

    cambios finales

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,6 +1,6 @@
 class Garage < ApplicationRecord
   belongs_to :user
-  has_many :reservation
+  has_many :reservation, dependent: :destroy
   validates :title, :description, :address, :availability, :phone_number, :dimensions, :price_per_day, :price_per_hour, presence: true
   validates :title, :address, uniqueness: true
   has_one_attached :photo
